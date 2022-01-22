0119a62c9f3745258cfe5820a951997860739a47
s.milanchev@abv.bg <GogetIt78>
2022-01-22 17:04:21 +0200

    changed navbar button colour

--- map_test/locations/forms.py
+++ map_test/locations/forms.py
@@ -9,13 +9,22 @@ class LocationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = ('address',)
+        widgets = {
+            'address',
+            forms.TextInput(
 
+        #         'style': ["justify - content: center",
+        # "align - items: center",
+        # "display: flex",
+        # "margin - right: 5px"]
+            )
+        }
 
-    def clean_address(self):
-        address = self.cleaned_data['address']
-        valid_address = geocoder.osm(address)
-        print(address)
-        if not valid_address:
-            raise ValidationError('No such address! Enter a valid one!')
+        def clean_address(self):
+            address = self.cleaned_data['address']
+            valid_address = geocoder.osm(address)
+            print(address)
+            if not valid_address:
+                raise ValidationError('No such address! Enter a valid one!')
 
-        return address
+            return address
